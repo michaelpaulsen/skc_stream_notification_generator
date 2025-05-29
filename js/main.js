2412c95bb1450e08bd5095c22fc715b0c4fc3cde
michaelpaulsen <michael.j.paulsen@gmail.com>
2025-05-28 23:19:34 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,12 +6,14 @@ function update(){
 	set_copy_value("#twitter_copy", non_discord);
 
 
-	//$("#discord_copy").attr("copy_value", discord)
 	$("#discord_out").html(replace_new_lines(discord));
 	$("#twitter_out").html(replace_new_lines(non_discord));
 }
+
 add_url_input("https://www.twitch.tv/skeleton_craft_gaming")
 update();
+
+
 $(".change").on("change", ()=>{
  update();
 });
@@ -19,7 +21,5 @@ $(".change").on("change", ()=>{
 $("#addURL").on("click", () => {
 	add_url_input()
 
-	let id = `url${url_count+1}`;
-	console.log(id);
 
 });
