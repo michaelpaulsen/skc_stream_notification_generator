a93723f938b369824f73d9a9684004bafb3ae6f0
michaelpaulsen <michael.j.paulsen@gmail.com>
2025-06-14 10:36:07 -0700

    added a key for the emoji.
    the fact that this is shown by default may change but I wanted to add a key for the emoji that I currently support.

--- js/main.js
+++ js/main.js
@@ -4,8 +4,6 @@ function update(){
 	let {non_discord, discord}  = template_parse(game);
 	set_copy_value("#discord_copy", discord);
 	set_copy_value("#twitter_copy", non_discord);
-
-
 	$("#discord_out").html(replace_new_lines(discord));
 	$("#twitter_out").html(replace_new_lines(non_discord));
 }
@@ -18,8 +16,27 @@ $(".change").on("input", ()=>{
  update();
 });
 
+
 $("#addURL").on("click", () => {
 	add_url_input()
+});
+//show and hide the key
+let hidden = false;
+let btn_toggle_keys = $("#key_hide_btn");
+btn_toggle_keys.on("click", ()=>{
+	hidden = !hidden;
+	$(`#emoji_key`).toggle();
+	if(hidden){
+		btn_toggle_keys.html("show keys");
+	}else{
+		btn_toggle_keys.html("hide keys");
 
+	}
+});
 
 });
+//Create the key for the emoji
+let key = $("#emoji_key")
+for(let x in emoji) {
+	key.append(`:${x}:  => ${emoji[x]}<br>`)
+}
